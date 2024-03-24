c4c12698e2c5af795915e1533086c04f00c14381
Telnesito <carlosj_1703@hotmail.com>
2024-03-24 16:14:40 -0400

    [FEAT] Se agrego la pantalla de carga principal

---
+++ app/utils/fuentes.js
@@ -0,0 +1,6 @@
+import { Fugaz_One } from "next/font/google"
+
+export const fugazOne = Fugaz_One({
+  subsets: ['latin'],
+  weight: '400'
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,34 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    colors: {
-      'azulMarino': '#100D40',
-      'Blanco': '#FAF9FF',
-      'BlancoClaro': '#FFFFFF80',
-      'MoradoClaro': '#F1EDFF',
-      'GrisClaro': '#F2F2F2',
-      'Gris': '#999999',
-      'Rojo': '#FF552F',
-      'NegroInputs': '#333333',
-      'GrisLabel': '#020614'
+      colors: {
+        'azulMarino': '#100D40',
+        'Blanco': '#FAF9FF',
+        'BlancoClaro': '#FFFFFF80',
+        'MoradoClaro': '#F1EDFF',
+        'GrisClaro': '#F2F2F2',
+        'Gris': '#999999',
+        'Rojo': '#FF552F',
+        'NegroInputs': '#333333',
+        'GrisLabel': '#020614'
 
+      },
+
+      keyframes: {
+        aparecer: {
+          '0%': {
+            transform: 'scale(.8)'
+          },
+
+
+          '100%': {
+            transform: 'scale(1)'
+          },
+        }
+      },
+      animation: {
+        'logo-aparecer': 'aparecer 2s infinite alternate',
+      },
     },
   },
   plugins: [],
