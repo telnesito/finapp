507b7da08b1f2cfb5e1a4e0ec4263b8321b468cd
Telnesito <carlosj_1703@hotmail.com>
2024-03-24 12:56:00 -0400

    [STYLE] Se agrego la paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'azulMarino': '#100D40',
+      'Blanco': '#FAF9FF',
+      'BlancoClaro': '#FFFFFF80',
+      'MoradoClaro': '#F1EDFF',
+      'GrisClaro': '#F2F2F2',
+      'Gris': '#999999',
+      'Rojo': '#FF552F',
+      'NegroInputs': '#333333',
+      'GrisLabel': '#020614'
+
+    },
   },
   plugins: [],
 };
