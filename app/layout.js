f038345f67009c61a7f2f776687f774576b55a75
Telnesito <carlosj_1703@hotmail.com>
2024-03-24 16:42:52 -0400

    [FEAT] Se agrego el redireccionamiento al onboarding

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" className="bg-azulMarino">
+    <html lang="es" >
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,13 +1,23 @@
+'use client'
+import { useRouter } from "next/navigation";
 import { fugazOne } from "./utils/fuentes";
 import Image from 'next/image'
+
+
 export default function Home() {
 
+  const router = useRouter()
+
+
+  setTimeout(() => {
+    router.push('onboarding')
+  }, 3000);
 
 
   return (
     <main className="h-screen min-h-max w-screen min-w-fit bg-azulMarino">
       <div className="animate-pulse flex flex-col items-center justify-center h-screen">
-        <Image className="animate-logo-aparecer relative left-[65px]" width={49} height={49} src={'./logoIcon.svg'}></Image>
+        <Image alt="Imagen circular con simbolo de dolar" className="animate-logo-aparecer relative left-[65px]" width={49} height={49} src={'./logoIcon.svg'}></Image>
         <h1 className={`text-Blanco text-[64px] ${fugazOne.className}`}>FINAPP</h1>
       </div>
     </main>
