73663e8f31203b3bd63cdf26ac99050246714c97
Telnesito <carlosj_1703@hotmail.com>
2024-03-25 15:50:28 -0400

    pruebax3

--- app/components/CardOnBoarding.jsx
+++ app/components/CardOnBoarding.jsx
@@ -1,52 +1,52 @@
-'use client'
-import React, { useState } from 'react'
-import Image from 'next/image'
-import Button from './Button'
-import { useRouter } from 'next/navigation'
-import { Popins } from '../utils/fuentes'
-import { onBoarding } from '../utils/onBoardingText'
-const CardOnBoarding = () => {
-  const [index, setIndex] = useState(0)
-  const router = useRouter()
+// 'use client'
+// import React, { useState } from 'react'
+// import Image from 'next/image'
+// import Button from './Button'
+// import { useRouter } from 'next/navigation'
+// import { Popins } from '../utils/fuentes'
+// import { onBoarding } from '../utils/onBoardingText'
+// const CardOnBoarding = () => {
+//   const [index, setIndex] = useState(0)
+//   const router = useRouter()
 
-  const handlePrimary = () => {
+//   const handlePrimary = () => {
 
-    if (index !== 2) {
-      setIndex((prev) => prev + 1)
-    } else {
-      router.push('/register')
-    }
-  }
+//     if (index !== 2) {
+//       setIndex((prev) => prev + 1)
+//     } else {
+//       router.push('/register')
+//     }
+//   }
 
-  const handleSecondary = () => {
+//   const handleSecondary = () => {
 
-    if (index !== 2) {
-      setIndex(2)
-    } else {
-      router.push('/login')
-    }
-  }
+//     if (index !== 2) {
+//       setIndex(2)
+//     } else {
+//       router.push('/login')
+//     }
+//   }
 
-  return (
-    <div className={`${Popins.className} flex flex-col min-h-max items-center justify-center gap-10 animate-fade-aparecer`}>
-      <Image alt='Imagenes de onboard' width={252} height={227} src={onBoarding[index].image}>
+//   return (
+//     <div className={`${Popins.className} flex flex-col min-h-max items-center justify-center gap-10 animate-fade-aparecer`}>
+//       <Image alt='Imagenes de onboard' width={252} height={227} src={onBoarding[index].image}>
 
-      </Image>
-      <h1 className='font-semibold text-center text-azulMarino text-[30px] '>{onBoarding[index].title}</h1>
-      <p className='text-center text-Gris'>{onBoarding[index].descripcion}</p>
-      <div className='flex gap-3'>
-        {onBoarding.map((obj, indexBoard) => <span onClick={() => setIndex(indexBoard)} key={indexBoard} className={`cursor-pointer rounded-full w-[12px] h-[12px] ${indexBoard === index ? 'bg-azulMarino' : 'bg-GrisClaro'}`}>
-        </span>)}
+//       </Image>
+//       <h1 className='font-semibold text-center text-azulMarino text-[30px] '>{onBoarding[index].title}</h1>
+//       <p className='text-center text-Gris'>{onBoarding[index].descripcion}</p>
+//       <div className='flex gap-3'>
+//         {onBoarding.map((obj, indexBoard) => <span onClick={() => setIndex(indexBoard)} key={indexBoard} className={`cursor-pointer rounded-full w-[12px] h-[12px] ${indexBoard === index ? 'bg-azulMarino' : 'bg-GrisClaro'}`}>
+//         </span>)}
 
-      </div>
+//       </div>
 
-      <div className=' sm:flex-col flex flex-col lg:flex-row'>
-        <Button type='contained' onClick={handlePrimary} value={onBoarding[index].primaryText} />
+//       <div className=' sm:flex-col flex flex-col lg:flex-row'>
+//         <Button type='contained' onClick={handlePrimary} value={onBoarding[index].primaryText} />
 
-        <Button type='text' onClick={handleSecondary} value={onBoarding[index].secondaryText} />
-      </div>
-    </div>
-  )
-}
+//         <Button type='text' onClick={handleSecondary} value={onBoarding[index].secondaryText} />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default CardOnBoarding
+// export default CardOnBoarding
