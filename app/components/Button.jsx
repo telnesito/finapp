938d4669badf633942d1aa058a8f0b4d3d8cf83e
Telnesito <carlosj_1703@hotmail.com>
2024-04-20 16:20:30 -0400

    form

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ value, type = 'outlined', onClick }) => {
+const Button = ({ value, type = 'outlined', onClick = () => true }) => {
   const handleClick = () => {
     onClick();
   };
@@ -8,7 +8,7 @@ const Button = ({ value, type = 'outlined', onClick }) => {
   const styleButton = type === 'contained' ? 'text-Blanco bg-azulMarino' : type === 'outlined' ? 'border border-azulMarino' : 'text-azulMarino underline'
 
   return (
-    <button className={`rounded-full h-[56px] w-[327px] font-semibold  ${styleButton}`} onClick={handleClick}>{value}</button>
+    <button type='submit' className={`rounded-full h-[56px] w-[327px] font-semibold  ${styleButton}`} onClick={handleClick}>{value}</button>
   )
 }
 
--- app/login/page.js
+++ app/login/page.js
@@ -23,23 +23,28 @@ const Page = () => {
           <Image alt="Imagen de login2" className="relative bottom-[60px]" width={164} height={164} src={'./layer2.svg'}></Image>
         </div>
       </div>
-      <div className='p-5 gap-[50px] min-h-[550px] flex flex-col items-center justify-center animate-fade-aparecer'>
-        <p className='font-semibold text-[24px] text-azulMarino'>Bienvenido de vuelta!</p>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        console.log('Hola')
+        router.push('/home')
+      }}>
+        <div className='p-5 gap-[50px] min-h-[550px] flex flex-col items-center justify-center animate-fade-aparecer'>
+          <p className='font-semibold text-[24px] text-azulMarino'>Bienvenido de vuelta!</p>
 
-        <div className='flex flex-col gap-3 w-full max-w-[700px]'>
-          <TextField type='email' label={'Correo electronico'} />
-          <TextField type='password' label={'Contraseña'} />
+          <div className='flex flex-col gap-3 w-full max-w-[700px]'>
+            <TextField type='email' label={'Correo electronico'} />
+            <TextField type='password' label={'Contraseña'} />
 
-          <p className='text-Gris text-right w-full text-[12px] '>Contraseña olvidada?</p>
+            <p className='text-Gris text-right w-full text-[12px] '>Contraseña olvidada?</p>
+          </div>
+
+          <div className='flex flex-col md:flex-row gap-[15px]'>
+            <Button value={'Iniciar sesion'} type='contained' />
+          </div>
+
+          <p className='text-azulMarino'>No tienes cuenta? <span onClick={() => router.push('/register')} className='text-azulMarino font-medium'>Registrate</span></p>
         </div>
-
-        <div className='flex flex-col md:flex-row gap-[15px]'>
-          <Button onClick={() => router.push('/home')} value={'Iniciar sesion'} type='contained' />
-
-        </div>
-
-        <p className='text-azulMarino'>No tienes cuenta? <span onClick={() => router.push('/register')} className='text-azulMarino font-medium'>Registrate</span></p>
-      </div>
+      </form>
     </div>
   )
 }
