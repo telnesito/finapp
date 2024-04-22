a627115604d571983f7695b05242727c8ba7b6c0
Telnesito <carlosj_1703@hotmail.com>
2024-04-21 20:46:13 -0400

    a

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ closeModal, isOpen, isClosing, children }) => {
   return (
     <>
       {isOpen && (
-        <div className="scroll- fixed inset-0 flex items-end justify-center z-50">
+        <div className="overflow-hidden fixed inset-0 flex items-end justify-center z-[450]">
           <div className=" absolute inset-0 bg-gray-900 bg-opacity-75" onClick={closeModal}></div>
           <div className={` ${!isClosing ? 'animate-bottom-top' : 'animate-top-bottom'}  h-[750px] absolute bg-white rounded-lg overflow-hidden shadow-xl transform transition-all w-full`}>
             <div className="p-6">
