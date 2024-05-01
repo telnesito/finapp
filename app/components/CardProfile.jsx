7d226fb464ac40fe3aa28a829ca06ebf0aec0527
Telnesito <carlosj_1703@hotmail.com>
2024-05-01 00:39:27 -0400

    DAtos del usuario y onclick en los botones de configurar

--- app/components/CardProfile.jsx
+++ app/components/CardProfile.jsx
@@ -1,14 +1,16 @@
+'use client'
+
 import React from 'react'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-const CardProfile = ({ Icon, text }) => {
+const CardProfile = ({ Icon, text, onClick }) => {
   return (
-    <div className='flex items-center drop-shadow-sm bg-white p-2 h-[80px] rounded-2xl justify-between'>
+    <button onClick={onClick} className='flex items-center drop-shadow-sm bg-white p-2 h-[80px] text-left rounded-2xl justify-between'>
       <div className='p-2 items-center flex justify-center h-[50px] w-[50px] rounded-full bg-[#F1EDFF]'>
         <Icon className='text-azulMarino text-[30px]' />
       </div>
       <p className='text-[#666666] w-[260px] font-normal text-[16px]'>{text}</p>
       <KeyboardArrowRightIcon className='text-Gris' />
-    </div>
+    </button>
   )
 }
 
--- app/perfil/page.js
+++ app/perfil/page.js
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import PersonIcon from '@mui/icons-material/Person';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
@@ -11,9 +12,23 @@ const Page = () => {
       {/* Cuenta */}
       <div className='flex flex-col animate-fade-aparecer bg-[#F9FAFC]'>
         <p className='text-azulMarino text-[18px] font-medium mb-'>Cuenta</p>
+        <div className='mt-[20px] mb-[20px]'>
+          <div className='rounded-xl flex h-[80px] items-center justify-between drop-shadow-sm bg-white p-2'>
+            <div className='p-2 items-center flex justify-center h-[50px] w-[50px] rounded-full bg-[#F1EDFF]'>
+              <PersonIcon className='text-azulMarino text-[30px]' />
+            </div>
+            <div className='flex flex-col items-start'>
+              <p className='text-azulMarino font-medium '>Carlos Ternera</p>
+              <p className='text-[12px]'>Carlosternera46@gmail.com</p>
+            </div>
+            <p className='p-1 border-2 text-[14px] border-azulMarino rounded-3xl w-[80px] text-center'>Basic</p>
+          </div>
+        </div>
+
+        <p className='text-azulMarino text-[18px] font-medium mb-'>Configuraciones</p>
         <div className='flex mt-[20px] gap-4 flex-col'>
 
-          <CardProfile Icon={PersonIcon} text={'Cambiar informacion personal'} />
+          <CardProfile onClick={() => console.log('Hola mundo')} Icon={PersonIcon} text={'Cambiar informacion personal'} />
 
           <CardProfile Icon={LockIcon} text={'Cambiar contraseña'} />
 
