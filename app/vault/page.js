13e744999b5d53aba803b74c6ca51ddbe6915bd6
kurco07 <pedrojuliolicc99@gmail.com>
2024-05-15 14:58:19 -0400

    me quedaron dos bugs. Uno cuando voy a cerrar el modal de detalles, por alguna razón se actualiza la página entonces se pierden los datos. El segundo bug es que al momento de editar la información de los usuarios no se ve reflejado en los formularios

--- app/vault/page.js
+++ app/vault/page.js
@@ -1,4 +1,5 @@
 'use client'
+
 import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -22,6 +23,7 @@ import BackPage from '../components/BackPage';
 
 const Page = () => {
   const { isOpen, openModal, isClosing, closeModal } = useModal();
+  //router que creo que no se usa pero ahí estaba
   const router = useRouter();
   
   // Estados para los valores del formulario
@@ -35,7 +37,7 @@ const Page = () => {
   const openEdicion = useModal();
   const openEliminar = useModal();
 
-  // Funciones para actualizar los estados 
+  // Funciones para actualizar los estados ó escribir en los formularios
   const handleUsuario = (value) => {
     setUsuario(value);
   };
@@ -52,22 +54,66 @@ const Page = () => {
     setConfirmar(value);
   };
 
-  // Función para eliminar la última fila de datos
-  const eliminarFila = () => {
-    const tableRows = document.querySelectorAll('tr');
-    const lastDataRow = tableRows[tableRows.length - 1];
-    if (lastDataRow) {
-      lastDataRow.remove();
-    }
-    openEliminar.closeModal();
+
+  // Función para eliminar usuario
+const eliminarFila = () => {
+  if (usuarios.length > 0) {
+    const usuariosActualizados = usuarios.slice(0, -1); 
+    setUsuarios(usuariosActualizados);
+  }
+  openEliminar.closeModal();
+};
+
+  // Almacenar informacion de los usuarios
+  const [usuarios, setUsuarios] = useState([]);
+  
+  // Guardar la información de los usuarios
+  const handleGuardar = (event) => {
+    event.preventDefault();
+    const nuevoUsuario = {
+      usuario,
+      correo,
+      contrasena
+    };
+    setUsuarios([...usuarios, nuevoUsuario]);
+    closeModal();
   };
+
+  // Estado para almacenar información en el modal de detalles
+  const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
+
+  // Mostrar la información del usuario en el modal de detalles
+  const abrirDetalles = (user) => {
+    setUsuarioSeleccionado(user);
+    openDetalles.openModal();
+
+  };
+
+
+// Guardar la información editada del usuario seleccionado no funciona no se por que
+const handleGuardarEdicion = (event) => {
+  event.preventDefault();
+  const usuarioEditado = {
+    usuario,
+    correo,
+    contrasena
+  };
+
+  // Buscar el usuario seleccionado en la lista de usuario
+  const usuariosActualizados = usuarios.map(user =>
+    user === usuarioSeleccionado ? usuarioEditado : user
+  );
+
+  setUsuarios(usuariosActualizados);
+  openEdicion.closeModal();
+};
 
   return (
     <Box p={'20px 20px'}>
       <BackPage destino={'perfil'} paginaActual={'Gestion cuentas'} />
 
       <TableContainer component={Paper}>
-        <Table sx={{}} aria-label="simple table">
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell align="left">Usuario</TableCell>
@@ -76,46 +122,24 @@ const Page = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell align="left">{'telnesito'}</TableCell>
-              <TableCell align="left">{'carlosternera46@gmail.com'}</TableCell>
-              <TableCell
-                align="center"
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  gap: '10px',
-                }}
-              >
-                <RemoveRedEyeIcon onClick={openDetalles.openModal} />
-                <EditIcon onClick={openEdicion.openModal} />
-                <DeleteIcon onClick={openEliminar.openModal} />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-          <TableBody>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell align="left">{'freedito'}</TableCell>
-              <TableCell align="left">{'carlosternera46@gmail.com'}</TableCell>
-              <TableCell
-                align="center"
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  gap: '10px',
-                }}
-              >
-                <RemoveRedEyeIcon onClick={openDetalles.openModal} />
-                <EditIcon onClick={openEdicion.openModal} />
-                <DeleteIcon onClick={openEliminar.openModal} />
-              </TableCell>
-            </TableRow>
+            {usuarios.map((user, index) => (
+              <TableRow key={index}>
+                <TableCell align="left">{user.usuario}</TableCell>
+                <TableCell align="left">{user.correo}</TableCell>
+                <TableCell align="center">
+                  <RemoveRedEyeIcon onClick={() => abrirDetalles(user)} />
+                  <EditIcon onClick={openEdicion.openModal}/>
+                  <DeleteIcon onClick={openEliminar.openModal} />
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
+
       <Box
         padding={'15px 15px'}
-        onClick={() => openModal()}
+        onClick={openModal}
         component={'button'}
         position={'fixed'}
         bottom={'20px'}
@@ -138,7 +162,7 @@ const Page = () => {
 
       {/* Modal Agregar */}
       <Modal isOpen={isOpen} isClosing={isClosing} closeModal={closeModal}>
-        <form onSubmit={() => console.log('Submiteado')} className='flex flex-col gap-3 '>
+        <form onSubmit={handleGuardar} className='flex flex-col gap-3 '>
           <Box
             display={'flex'}
             component={'button'}
@@ -177,19 +201,37 @@ const Page = () => {
             <KeyboardArrowLeftIcon />
             <p className='text-azulMarino text-[18px] font-medium '>Detalles de la cuenta</p>
           </Box>
-          <TextField type='text' label={'Nombre de usuario'} />
-          <TextField type='email' label={'Correo electronico'} />
-          <TextField type='password' label={'Contraseña'} />
+          <TextField 
+            type='text' 
+            label={'Nombre de usuario'} 
+            defaultValue={usuarioSeleccionado ? usuarioSeleccionado.usuario : ''} 
+            readOnly 
+          />
+          <TextField 
+            type='email' 
+            label={'Correo electronico'} 
+            defaultValue={usuarioSeleccionado ? usuarioSeleccionado.correo : ''} 
+            readOnly 
+          />
+
+          <TextField type='text' 
+          label={'Contraseña'} 
+          defaultValue={usuarioSeleccionado ? usuarioSeleccionado.contrasena : ''} 
+          readOnly   />
 
           <div className='flex items-center justify-center mt-6'>
-            <Button onClick={() => openDetalles.closeModal()} value={'Cerrar'} type='contained' />
+          <Button onClick={(e) => {
+            e.preventDefault(); // Evitar el comportamiento predeterminado
+            e.stopPropagation(); // Detener la propagación del evento
+            openDetalles.closeModal();
+          }} value={'Cerrar'} type='contained' />
           </div>
         </form>
       </Modal>
 
       {/* Modal Edicion */}
       <Modal isOpen={openEdicion.isOpen} isClosing={openEdicion.isClosing} closeModal={openEdicion.closeModal}>
-        <form onSubmit={() => console.log('Submiteado')} className='flex flex-col gap-3 '>
+        <form onSubmit={handleGuardarEdicion} className='flex flex-col gap-3 '>
           <Box
             display={'flex'}
             component={'button'}
@@ -202,16 +244,38 @@ const Page = () => {
             <KeyboardArrowLeftIcon />
             <p className='text-azulMarino text-[18px] font-medium '>Editar información</p>
           </Box>
-          <TextField type='text' label={'Nombre de usuario'} />
-          <TextField type='email' label={'Correo electronico'} />
-          <TextField type='password' label={'Nueva contraseña'} />
-          <TextField type='password' label={'Confirmar nueva contraseña'} />
+          <TextField
+            type='text'
+            label={'Nombre de usuario'}
+            defaultValue={usuario}
+            onChange={(value) => handleUsuario(value)}
+          />
+          <TextField
+            type='email'
+            label={'Correo electronico'}
+            defaultValue={correo}
+            onChange={(value) => handleCorreo(value)}
+          />
+          <TextField
+            type='password'
+            label={'Nueva contraseña'}
+            defaultValue={contrasena}
+            onChange={(value) => handleContrasena(value)}
+          />
+          <TextField
+            type='password'
+            label={'Confirmar nueva contraseña'}
+            defaultValue={confirmar}
+            onChange={(value) => handleConfirmar(value)}
+          />
 
           <div className='flex items-center justify-center mt-6'>
             <Button value={'Guardar'} type='contained' />
           </div>
         </form>
       </Modal>
+
+
 
       {/* Modal Eliminar */}
       <Modal isOpen={openEliminar.isOpen} isClosing={openEliminar.isClosing} closeModal={openEliminar.closeModal}>
@@ -238,5 +302,4 @@ const Page = () => {
   );
 };
 
-export default Page;
-
+export default Page; 
