f0be353c3a2ae31558505e4ae1226e94a83ebe64
Telnesito <carlosj_1703@hotmail.com>
2024-05-21 03:40:23 -0400

    Chat bot

--- app/chatbot/page.js
+++ app/chatbot/page.js
@@ -1,96 +1,61 @@
-"use client"
-import React, { useEffect, useState, useRef } from 'react';
-import { TextField, IconButton, InputAdornment } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
+"use client";
+import React from 'react';
+
+import { useChat } from "ai/react";
 
 const Page = () => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-  const messageContainerRef = useRef(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    sendMessage();
-  };
-
-  const sendMessage = () => {
-    if (message.trim() !== '') {
-      setMessages([...messages, { text: message, maxWidth: calculateMaxWidth(message.length) }]);
-      setMessage('');
-    }
-  };
-
-  // ancho de los mensajes
-  const calculateMaxWidth = (messageLength) => {
-    const basePercentage = 30;
-    const incrementPercentage = 22;
-
-    let maxWidthPercentage;
-
-
-    if (messageLength >= 1 && messageLength <= 6) {
-      maxWidthPercentage = 20;
-    } else {
-
-      const numberOfIncrements = Math.ceil(messageLength / 20);
-      maxWidthPercentage = basePercentage + incrementPercentage * numberOfIncrements;
-    }
-
-    return Math.min(maxWidthPercentage, 100);
-  };
-
-
-
-
-  useEffect(() => {
-    if (messageContainerRef.current) {
-      messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
-    }
-  }, [messages]);
+  const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat(); // Ruta corregida
 
   return (
     <div className='p-5 min-h-[760px] bg-[#F9FAFC]'>
       <div className='flex flex-col animate-fade-aparecer bg-[#F9FAFC]' style={{ minHeight: '100vh', paddingBottom: '80px' }}>
         <p className='text-azulMarino text-[18px] font-medium mb-'>Chatbot</p>
-        {/* divider */}
         <div className='h-[1px] mt-[5px] mb-[5px] bg-gray-100'></div>
 
-        {/* enviar mensaje */}
-        <div className='flex flex-col mt-[20px]' style={{ position: 'fixed', bottom: 80, left: 0, width: '100%' }}>
-          <ul ref={messageContainerRef} className='h-[70vh] flex flex-col-reverse overflow-auto'>
-            {messages.slice(0).reverse().map((message, index) => (
-              <li key={index} className='p-2 m-1 rounded-lg text-white' style={{ maxWidth: `${message.maxWidth}%`, wordWrap: 'break-word', backgroundColor: '#001F3F' }}>{message.text}</li>
+        <form onSubmit={handleSubmit} className="max-w-xl w-full">
+          <div className="text-white max-h-96 h-full overflow-y-auto">
+            {messages.map((m) => (
+              <div
+                key={m.id}
+                className={`flex flex-col mb-2 p-2 rounded-md ${m.role === "assistant"
+                  ? "self-end bg-gray-800"
+                  : "self-start bg-blue-700"
+                  }`}
+              >
+                <span
+                  className={`text-xs ${m.role === "assistant" ? "text-right" : "text-left"
+                    }`}
+                >
+                  {m.role}
+                </span>{" "}
+                {m.content}
+              </div>
             ))}
-          </ul>
+          </div>
 
-          <form onSubmit={handleSubmit} className="w-full">
-            <TextField
-              onChange={(e) => setMessage(e.target.value)}
-              value={message}
-              placeholder={'Escribe un mensaje'}
-              type='text'
-              fullWidth
-              sx={{
-                '& fieldset': {
-                  borderColor: '#001F3F !important',
-                },
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <IconButton type='submit' sx={{ color: '#001F3F' }}>
-                      <SendIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </form>
-        </div>
+          <div className="flex justify-between my-4">
+            <label className="text-white block font-bold my-2">
+              Say something...
+            </label>
+            <button
+              className="bg-blue-600 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:opacity-50"
+              disabled={isLoading || !input}
+            >
+              Send
+            </button>
+          </div>
+          <textarea
+            rows={4}
+            value={input}
+            onChange={handleInputChange}
+            className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+            placeholder="Type something..."
+            autoFocus
+          />
+        </form>
       </div>
     </div>
   );
-
 };
 
 export default Page;
