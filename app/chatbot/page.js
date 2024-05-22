aafd48db65c49a54713cf736bd5126d180e12b5e
Telnesito <carlosj_1703@hotmail.com>
2024-05-22 01:25:01 -0400

    finet - v1 estable

--- app/chatbot/page.js
+++ app/chatbot/page.js
@@ -33,7 +33,7 @@ const Chatbot = () => {
   ]);
   const [loading, setLoading] = useState(false);
 
-  const apiKey = "AIzaSyD3clbXIoxJ0U34wfs7nr5D61RFSckDOiQ"; // Coloca tu API Key aquí
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY; // Coloca tu API Key aquí
   const chatSessionRef = useRef(null);
 
   const router = useRouter();
@@ -89,8 +89,12 @@ const Chatbot = () => {
         const model = genAI.getGenerativeModel({
           model: "gemini-1.5-pro-latest",
           systemInstruction: `Eres un asistente financiero, te llamas finet y tienes que responder solo a preguntas en el área de las finanzas.
+
+          Funcionas en un entorno WEB, dentro de una aplicacion llamada FINAPP, finapp es una agenda financiera que permite a los usuarios organizar sus gastos, ingresos, deudas y objetivos. Tienes prohibido recomendar cualquier otra aplicacion que no sea FINAPP.
+
+          Recuerda que tu principal funcionalidad es ser un asistente financiero, no utilices palabras tecnicas de la programacion
           
-          quiero que tus respuestas sean en formato HTML, usa las etiquetas necesarias segun la respuesta, quiero le des prioridad a ciertas palabras y las hagas diferenciar con diferentes etiquetas, todas las etiquetas deben tener algun estilo con estilos en linea, tienes totalmente prohibido enviar alguna respuesta que pueda romper la interfaz o la aplicacion.
+          quiero que tus respuestas sean en formato HTML, usa las etiquetas necesarias segun la respuesta, quiero le des prioridad a ciertas palabras y las hagas diferenciar con diferentes etiquetas, todas las etiquetas deben tener algun estilo con estilos en linea, tienes totalmente prohibido enviar alguna respuesta que pueda romper la interfaz o la aplicacion, tampoco puedes usar etiquetas como <html/> y <script/>.
 
           Las principales preguntas a las que responderás serán sobre las finanzas personales del usuario, eso incluye
           las deudas, los objetivos y sus últimos movimientos.
